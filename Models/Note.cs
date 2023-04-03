fb04ed0d0169d1dd90c977ebbb09e17f8c4548dc
MartySenpai <112476929+MartySenpai@users.noreply.github.com>
2023-04-03 12:40:23 +0200

    Project creation and basic outline

---
+++ Models/Note.cs
@@ -0,0 +1,8 @@
+namespace NoteTakingApp;
+
+class Note  
+{
+    public string Title { get; set; }
+
+    public string Content { get ; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace NoteTakingApp;
+
+// Welcome user.
+
+// Swtich menau for app options(read, write, save and load).
+
+// Refactor when it make sense, don't try to do everything at the start!.
